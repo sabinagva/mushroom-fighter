4f0c52b59d265cbc2846968b6d2e53f289eca3db
Sabina Gahramanova <sabina.ghrmnli@gmail.com>
2023-05-08 22:48:33 -0500

    gave buttons id and set if statement for hp decrease

--- script.js
+++ script.js
@@ -6,6 +6,13 @@ $(document).ready(onReady);
 // let fungusHP = 100;
 
 function onReady() {
+    $('#scepter').on('click',scepterAttackBtn);
+    $('#entangle').on('click',entangleAttackBtn);
+    $('#dragon').on('click',dragonAttackBtn);
+    $('#star').on('click',starAttackBtn);
+
+
+
     
     // Make sure you check the index.html file! 
     // There are lots of buttons and things ready for you to hook into here!
@@ -15,4 +22,66 @@ function onReady() {
     // - Handle events that ->
     // - Updates state which is ->
     // - Rendered to the DOM
+}
+let mushroomHP = 100;
+const scepterDamage = 14;
+const entangleDamage = 9;
+const dragonDamage = 47;
+const starDamage = 25;
+function scepterAttackBtn() {
+    console.log('in scepterAttackBtn')
+   
+    if((mushroomHP - scepterDamage) <= 0) {
+        mushroomHP = 0;
+    }
+    else {
+        mushroomHP -= scepterDamage
+    
+    }
+    
+    $('#hp-meter').val(mushroomHP)
+    $('.hp-text').text(mushroomHP + ' HP')
+   
+
+}
+function entangleAttackBtn() {
+
+    if((mushroomHP - entangleDamage) <= 0) {
+        mushroomHP = 0;
+    }
+    else {
+        mushroomHP -= entangleDamage
+    
+    }
+    
+    $('#hp-meter').val(mushroomHP)
+    $('.hp-text').text(mushroomHP + ' HP')
+
+}
+function dragonAttackBtn() {
+
+    if((mushroomHP - dragonDamage) <= 0) {
+        mushroomHP = 0;
+    }
+    else {
+        mushroomHP -= dragonDamage
+    
+    }
+    
+    $('#hp-meter').val(mushroomHP)
+    $('.hp-text').text(mushroomHP + ' HP')
+
+}
+function starAttackBtn() {
+
+    if((mushroomHP - starDamage) <= 0) {
+        mushroomHP = 0;
+    }
+    else {
+        mushroomHP -= starDamage
+    
+    }
+    $('#hp-meter').val(mushroomHP)
+    $('.hp-text').text(mushroomHP + ' HP')
+
 }
